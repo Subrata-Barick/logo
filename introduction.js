36c109695be4dff7e5c9de3a4155b47deddb22d6
my-first-project <subrata.barick27@gmail.com>
2023-11-16 16:30:21 +0530

    first change

---
+++ introduction.js
@@ -0,0 +1,14 @@
+console.log("hello world")
+var num1=56;
+var num2=67;
+console.log("the value of num1+num2 is"+(num1+num2));
+console.log("the value of num1-num2 is"+(num1-num2));
+console.log("the value of num1*num2 is"+(num1*num2));
+var firstname = "subrata";
+document.write("my name is ");
+document.write(firstname);
+document.write("my name is ");
+document.write(firstname);
+document.write("my name is ");
+document.write(firstname);
+
